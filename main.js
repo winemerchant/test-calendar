5e11d61b92ad86d16e32fb2f1143497feb9334e5
Anastasiia Kozhara <94788527+winemerchant@users.noreply.github.com>
2022-04-25 04:30:56 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,68 +2,88 @@ var app = new Vue({
     el: '#calendar',
     data: function() {
         return {
+            //текущие год, месяц и день
             year: new Date().getFullYear(),
             month: new Date().getMonth(),
             date: new Date().getDate(),
+            //дни недели
             days: ['Sn', 'Mn', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            //массив для дней месяца
             daysInMonth: [],
             monthes: ['January', 'Fabruary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            //дата, выбранная пользователем
             chosenDate: null,
-            currentDate: [new Date().getFullYear(), new Date().getMonth(), new Date().getDate()]
+            //текущая дата
+            currentDate: [new Date().getFullYear(), new Date().getMonth(), new Date().getDate()],
+            lang: 'en',
+            textForDate: 'Current Date', 
+            textForLang: 'Change Language'
         }
     },
-
     mounted() {
+        //отрисовывает календарь
         this.getDaysInMonth()
-        // setTimeout(this.chooseCurrentData, 1000)
-        
+        //окрашивает ячейку с текущей датой  
+        this.$nextTick(this.colorCurrentDate)
     },
-            
     methods: {  
         getDaysInMonth() {
+            this.daysInMonth = []
             let allDays = []
             let daysCounter = 0
-            let weak = []
+            let week = []
+            //какой день недели был 1го числа текущего или выбранного месяца
             let dayOfFirstDate = new Date (this.year, this.month, 1).getDay()
+            if (this.lang == 'ru') {
+                dayOfFirstDate += 6
+            }
+            //какой день последний в текущем или выбранном месяце
             let lastDateOfMonth = new Date(this.year, this.month + 1, 0).getDate()
-            
+            //создает массив чисел - дней месяца
             for (i = 1; i < lastDateOfMonth + 1; i++) {
                 allDays.push(i)
-                
             }
-
-            if (dayOfFirstDate != 1){
-                for(i = 0; i < dayOfFirstDate - 1; i++) {
-                    allDays.unshift('')
-                }
+            //если месяц начинается не с понедельника, добавляет нужное количество "пустых" дней
+            // if (dayOfFirstDate != 0){
+            //     for(i = 0; i < dayOfFirstDate; i++) {
+            //         allDays.unshift('')
+            //     }
+            // }
+            for (i = 0; i < (dayOfFirstDate % 7); i++) {
+                allDays.unshift('')
             }
-
+            //присваивает каждые 7 дней в массив week и помещает его в массив dayInMonth
             for (i = 1; i < allDays.length + 1; i++) {
-                weak.push(allDays[daysCounter])
+                week.push(allDays[daysCounter])
                 daysCounter++
                 if (i % 7 == 0) {
-                    this.daysInMonth.push(weak)
-                    weak = []
+                    this.daysInMonth.push(week)
+                    week = []
                 }
             }
-
-            this.daysInMonth.push(weak)
-            
-            
-
+            //оставшиеся дни недели (если в последней неделе меньше 7ми дней)
+            this.daysInMonth.push(week)
         },
-
+        //выбрать предыдущий месяц
         choosePreviousMonth() {
+            let elem = document.getElementById(String(new Date().getDate()))
+            elem.style.borderWidth = '0px'
             this.daysInMonth = []
             this.month--
             if (this.month < 0) {
                 this.month = 11
                 this.year--
             }
+            
             this.getDaysInMonth()
+            if(this.month == new Date().getMonth()) {
+                this.$nextTick(this.colorCurrentDate)
+            }
         },
-
+        //выбрать следующий месяц
         chooseNextMonth() {
+            let elem = document.getElementById(String(new Date().getDate()))
+            elem.style.borderWidth = '0px'
             this.daysInMonth = []
             this.month++
             if (this.month > 11) {
@@ -71,22 +91,47 @@ var app = new Vue({
                 this.year++
             }
             this.getDaysInMonth()
+            if(this.month == new Date().getMonth()) {
+                this.$nextTick(this.colorCurrentDate)
+            }
         },
-
+        //окрашивает текущую дату
+        colorCurrentDate() {
+                let elem = document.getElementById(String(new Date().getDate()))
+                elem.style.borderWidth = '2px'
+                elem.style.borderColor = 'green' 
+        },
+        //меняет выбранную дату и окрашивает ее
         changeDate(e) {
             if(this.chosenDate != null) {
                 this.chosenDate.style.background = 'white'
             } 
             this.chosenDate = e.currentTarget
+            console.log(this.chosenDate)
             this.date = e.target.innerHTML
-            this.chosenDate.style.background = 'red'
+            this.chosenDate.style.background = 'lime'
             this.currentDate  = String(this.year) + '/' + String(this.month) + '/' + String (this.date)
-            console.log(this.currentDate)
-            
+            console.log(this.currentDate) 
         },
-
-
-    },
-
-    
+        changeLang: function(event) {
+            let elem = document.getElementById(String(new Date().getDate()))
+            elem.style.borderWidth = '0px'
+            if(event.target.value == 'en') {
+                this.lang = 'en'
+                this.days = ['Sn', 'Mn', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+                this.textForDate = 'Current Date' 
+                this.textForLang = 'Change Language'
+                this.$nextTick(this.getDaysInMonth)
+            } else if (event.target.value == 'ru') {
+                this.lang = 'ru'
+                this.days = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+                this.textForDate = 'Текущая дата' 
+                this.textForLang = 'Сменить язык'
+                this.$nextTick(this.getDaysInMonth)
+            }
+            setTimeout(() => {
+                this.colorCurrentDate()
+            }, 1);
+        } 
+    },   
 })
